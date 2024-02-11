4679a1e8cff53ae0c72580193da181b0b5f7429b
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-02-11 02:24:43 -0300

    add react, navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            keyframes: {
+                jumbo: {
+                    from: { "background-position": "50% 50%, 50% 50%" },
+                    to: { "background-position": "350% 50%, 350% 50%" },
+                },
+            },
+        },
+    },
+    darkMode: "class",
+    plugins: [nextui()],
+};
